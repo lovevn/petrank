2ce3450adcab0054059f44819669f4622f4877fc
Sam Ireland <sam.ireland.uk@gmail.com>
2018-07-21 21:45:38 +0100

    Prevent duplicate images being sent

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
+import random
 from django.shortcuts import render, redirect, get_object_or_404
 from pets.models import Pet
 
 def root(request):
 
-    pets = Pet.objects.order_by("?").all()[:2]
-    return render(request, "home.html", {"pets": pets})
+    pets = list(Pet.objects.all())
+    pet1 = random.choice(pets)
+    pets.remove(pet1)
+    pet2 = random.choice(pets)
+    return render(request, "home.html", {"pets": [pet1, pet2]})
