7a7e98aeffc9b999abe6d3401d4350f08fda1193
Sam Ireland <sam.ireland.uk@gmail.com>
2018-07-21 19:26:18 +0100

    Basic django app

---
+++ core/settings.py
@@ -0,0 +1,34 @@
+import os
+import binascii
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = binascii.hexlify(os.urandom(24)).decode()
+
+VERSION = "0.1.0"
+
+DATABASES = {"default": {
+ "ENGINE": "django.db.backends.sqlite3",
+ "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+}}
+
+ALLOWED_HOSTS = []
+DEBUG = True
+
+ROOT_URLCONF = "core.urls"
+
+INSTALLED_APPS = [
+ "django.contrib.contenttypes",
+ "django.contrib.staticfiles",
+ "django.contrib.humanize",
+ "core",
+]
+
+MIDDLEWARE = [
+ "django.contrib.sessions.middleware.SessionMiddleware",
+ "django.middleware.common.CommonMiddleware",
+ "django.middleware.csrf.CsrfViewMiddleware",
+]
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, "../static"))
