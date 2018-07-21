6378f57b1f95e3129e54b3c9286c298d46dd3777
Sam Ireland <sam.ireland.uk@gmail.com>
2018-07-21 21:35:59 +0100

    Randomise pet images

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from pets.models import Pet
 
 def root(request):
-    return render(request, "home.html")
+
+    pets = Pet.objects.order_by("?").all()[:2]
+    return render(request, "home.html", {"pets": pets})
