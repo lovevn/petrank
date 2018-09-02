43c4ba29a089ab7da5cabaaec0a7bc29883a0e2c
Sam Ireland <sam.ireland.uk@gmail.com>
2018-09-02 11:16:52 +0100

    Fix pet ID 500

--- pets/views.py
+++ pets/views.py
@@ -1,12 +1,15 @@
 """Pet specific views."""
 
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404
 from .models import Pet, PetSnapshot
 
 def pet(request, id):
     """The view to look at a particular pet image and its stats."""
 
-    pet = get_object_or_404(Pet, id=id)
+    try:
+        pet = get_object_or_404(Pet, id=id)
+    except ValueError: raise Http404()
     snapshots = PetSnapshot.objects.filter(pet=pet)
     time_data = [[int(s.datetime.strftime('%s')) * 1000, s.elo_rating] for s in snapshots]
     data = [d[1] for d in time_data]
