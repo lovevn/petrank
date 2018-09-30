c80df2a832ce63b39550cf404e502cf4d8c15f3b
Sam Ireland <sam.ireland.uk@gmail.com>
2018-09-30 15:57:28 +0100

    Fix rank problem

--- pets/views.py
+++ pets/views.py
@@ -12,12 +12,17 @@ def pet(request, id):
         pet = get_object_or_404(Pet, id=id)
     except ValueError: raise Http404()
     snapshots = PetSnapshot.objects.filter(pet=pet)
+
     time_data = [[int(s.datetime.strftime('%s')) * 1000, s.elo_rating] for s in snapshots]
     if time_data: time_data += [[int(datetime.now().strftime('%s')) * 1000, time_data[-1][1]]]
     data = [d[1] for d in time_data]
     time_rank_data = [[int(s.datetime.strftime('%s')) * 1000, s.ranking] for s in snapshots if s.ranking != 0]
-    if time_rank_data: time_rank_data += [[int(datetime.now().strftime('%s')) * 1000, time_rank_data[-1][1]]]
+    if time_rank_data:
+        time_rank_data += [[int(datetime.now().strftime('%s')) * 1000, time_rank_data[-1][1]]]
+    else:
+        time_rank_data = [[int(datetime.now().strftime('%s')) * 1000, 0]]
     rank_data = [d[1] for d in time_rank_data]
+    print(time_rank_data)
     defeats = [s.lost_against.name if s.lost_against else "" for s in snapshots]
     wins = [s.won_against.name if s.won_against else "" for s in snapshots]
     return render(request, "pet.html", {
