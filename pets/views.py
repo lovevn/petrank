a9673bddec29951f2c1f463d5f57392aa6d06c67
Sam Ireland <sam.ireland.uk@gmail.com>
2018-09-30 15:39:59 +0100

    Make current time appear on charts

--- pets/views.py
+++ pets/views.py
@@ -1,5 +1,6 @@
 """Pet specific views."""
 
+from datetime import datetime
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
 from .models import Pet, PetSnapshot
@@ -12,8 +13,10 @@ def pet(request, id):
     except ValueError: raise Http404()
     snapshots = PetSnapshot.objects.filter(pet=pet)
     time_data = [[int(s.datetime.strftime('%s')) * 1000, s.elo_rating] for s in snapshots]
+    time_data += [[int(datetime.now().strftime('%s')) * 1000, time_data[-1][1]]]
     data = [d[1] for d in time_data]
     time_rank_data = [[int(s.datetime.strftime('%s')) * 1000, s.ranking] for s in snapshots if s.ranking != 0]
+    time_rank_data += [[int(datetime.now().strftime('%s')) * 1000, time_rank_data[-1][1]]]
     rank_data = [d[1] for d in time_rank_data]
     defeats = [s.lost_against.name if s.lost_against else "" for s in snapshots]
     wins = [s.won_against.name if s.won_against else "" for s in snapshots]
